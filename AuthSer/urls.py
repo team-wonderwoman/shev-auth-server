a9bb8f7f462fb7d5d19feaeca076a28003464212
YejinChoi <cyj95428@naver.com>
2018-01-25 20:42:25 +0900

    SignUp구현

--- AuthSer/urls.py
+++ AuthSer/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+# AuthSer/urls.py
+
 urlpatterns = [
-    url(r'^signup/$', views.signup),
+    url(r'^signup/$', views.SignUpAPIView.as_view()),
+    #url(r'^login/$',views.login),
+
 ]
--- shev_v1/urls.py
+++ shev_v1/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+# shev_v1/urls.py
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
